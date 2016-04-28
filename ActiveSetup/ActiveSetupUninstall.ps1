cd88a69e993b7dac3251cea681e5fdaf5c0d703d
Stefan Zawacki <s.zawacki@oraylis.de>
2016-04-28 10:14:16 +0200

    Create ActiveSetupUninstall.ps1

---
+++ ActiveSetup/ActiveSetupUninstall.ps1
@@ -0,0 +1,16 @@
+<#
+
+    .SYNOPSIS
+
+    Empty template for active setp uninstallation.
+
+
+
+    .DESCRIPTION
+
+
+    Use this template for performing actions for users not logged in during uninstallation.
+    No parameters are passed by the main script.
+    Use current user context to access user settings.
+    
+#>
