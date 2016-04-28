71c7f34b6be5b5dad30cee2954afc5e91ffcbd3f
Stefan Zawacki <s.zawacki@oraylis.de>
2016-04-28 15:31:56 +0200

    Create Enable-IEESC.ps1

---
+++ Enable-IEESC.ps1
@@ -0,0 +1,35 @@
+function Enable-IEESC
+{
+    <#
+
+        .SYNOPSIS
+
+        Disable Internet Explorer Enhanced Security.
+
+
+
+        .DESCRIPTION
+
+        Disable Internet Explorer Enhanced Security on Microsoft Windows Servers.
+
+
+        .EXAMPLE 
+
+        Disable-IEESC
+
+    #>
+
+    try
+    {
+        $RegKey1 = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
+        $RegKey2 = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
+        Set-ItemProperty -Path $RegKey1 -Name “IsInstalled” -Value 1
+        Set-ItemProperty -Path $RegKey2 -Name “IsInstalled” -Value 1
+        Stop-Process -Name Explorer
+        Write-Host “IE Enhanced Security has been enabled.” -ForegroundColor Green
+    }
+    catch
+    {
+        Write-Host “Failed to enable IE Enhanced Security.” -ForegroundColor Red
+    }
+}
