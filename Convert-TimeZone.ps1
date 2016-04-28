896b5f213160f6d6a8b4e72e3963c7b87b026b1a
Stefan Zawacki <s.zawacki@oraylis.de>
2016-04-28 16:02:23 +0200

    Update Convert-TimeZone.ps1
    Add help section..

--- Convert-TimeZone.ps1
+++ Convert-TimeZone.ps1
@@ -1,11 +1,52 @@
 Function Convert-TimeZone 
-{  
+{
+    <#
+
+        .SYNOPSIS
+
+        Convert date time to given time zone.
+
+
+
+        .DESCRIPTION
+
+
+        This function convertes a DateTime-Object to a given time zone.
+
+
+
+        .PARAMETER DateTime 
+
+        A DateTime Object to convert.
+
+
+
+        .PARAMETER ToTimeZone 
+
+        The String representation of the time zone to convert to.
+
+
+
+        .EXAMPLE 
+
+        Convert-TimeZone -DateTime (Get-Date).ToUniversalTime() -ToTimeZone “W. Europe Standard Time”
+        
+        
+
+        .NOTES
+        
+        To receive a list of all available time zones run the following command:
+        [System.TimeZoneInfo]::GetSystemTimeZones() 
+
+    #>
+      
     [CmdletBinding()]            
     Param(  [Parameter(Mandatory=$true)]            
             [ValidateNotNullOrEmpty()]            
             [DateTime]$DateTime,
-            [parameter( Mandatory=$true)]            
-            [ValidateNotNullOrEmpty()]   
+
+            [Parameter(Mandatory=$true)]            
+            [ValidateNotNullOrEmpty()]              
             [String]$ToTimeZone  = ([System.TimeZoneInfo]::UTC).id            
     )            
 
@@ -13,6 +54,3 @@ Function Convert-TimeZone
              
     [System.TimeZoneInfo]::ConvertTime($DateTime, $ToTimeZoneObj)                     
 }
-
-$DateTime = Convert-TimeZone -DateTime (Get-Date).ToUniversalTime() -ToTimeZone “W. Europe Standard Time” 
-$DateTime.ToString("dd.MM.yyyy - HH:mm:ss")
