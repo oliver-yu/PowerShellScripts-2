2b150ade3c0da9df78a6e4e4fb62f05f6e2395b0
Stefan Zawacki <s.zawacki@oraylis.de>
2016-04-28 15:09:46 +0200

    Update and rename IsAdmin.ps1 to Is-Admin.ps1

--- Is-Admin.ps1
+++ Is-Admin.ps1
@@ -1,4 +1,4 @@
-Function IsAdmin
+Function Is-Admin
 {
     # Returns true/false
     ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
