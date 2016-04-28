c13c54e38c6d1e800f29291c29b35d7ade657eb2
Stefan Zawacki <s.zawacki@oraylis.de>
2016-04-28 10:13:53 +0200

    Create ActiveSetupInstall.ps1

---
+++ ActiveSetup/ActiveSetupInstall.ps1
@@ -0,0 +1,16 @@
+<#
+
+    .SYNOPSIS
+
+    Empty template for active setp installation.
+
+
+
+    .DESCRIPTION
+
+
+    Use this template for performing actions for users not logged in during installation.
+    No parameters are passed by the main script.
+    Use current user context to access user settings.
+    
+#>
