a4dd698e0c91a0ac93108a957d7d888353b81b58
Stefan Zawacki <s.zawacki@oraylis.de>
2016-05-17 10:13:14 +0200

    Update Disable-IEESC.ps1
    Add turn off Internet Explorer protected mode.

--- Disable-IEESC.ps1
+++ Disable-IEESC.ps1
@@ -13,18 +13,35 @@ function Disable-IEESC
         Disable Internet Explorer Enhanced Security on Microsoft Windows Servers.
 
 
+        .Parameter TurnOffProtectedMode
+        
+        Disable Internet Explorer Protected Mode.
+        
+        
+
         .EXAMPLE 
 
         Disable-IEESC
 
     #>
 
+    Param([switch]$TurnOffProtectedMode
+    )
+    
     try
     {
         $RegKey1 = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
         $RegKey2 = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
+        $RegKey3 = “HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3”
+        
         Set-ItemProperty -Path $RegKey1 -Name “IsInstalled” -Value 0
         Set-ItemProperty -Path $RegKey2 -Name “IsInstalled” -Value 0
+        
+        if ($TurnOffProtectedMode)
+        {
+            Set-ItemProperty -Path $RegKey3 -Name “2500” -Value 3
+        }
+        
         Stop-Process -Name Explorer
         Write-Host “IE Enhanced Security has been disabled.” -ForegroundColor Green
     }
