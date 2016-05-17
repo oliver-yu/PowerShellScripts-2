8a2e0967d8c2fd2ce6ff64bb1bceacc7ca74a37d
Stefan Zawacki <s.zawacki@oraylis.de>
2016-05-17 10:15:57 +0200

    Update Enable-IEESC.ps1
    Add turn on Internet Explorer protected mode.

--- Enable-IEESC.ps1
+++ Enable-IEESC.ps1
@@ -13,18 +13,36 @@ function Enable-IEESC
         Disable Internet Explorer Enhanced Security on Microsoft Windows Servers.
 
 
+
+        .PARAMETER TurnOnProtectedMode
+        
+        Enable Internet Explorer Protected Mode.
+        
+        
+
         .EXAMPLE 
 
-        Disable-IEESC
+        Enable-IEESC -TurnOnProtectedMode
 
     #>
+
+    Param([switch]$TurnOnProtectedMode
+    )
 
     try
     {
         $RegKey1 = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
         $RegKey2 = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
+        $RegKey3 = “HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3”
+        
         Set-ItemProperty -Path $RegKey1 -Name “IsInstalled” -Value 1
         Set-ItemProperty -Path $RegKey2 -Name “IsInstalled” -Value 1
+        
+        if ($TurnOnProtectedMode)
+        {
+            Set-ItemProperty -Path $RegKey3 -Name “2500” -Value 0
+        }
+        
         Stop-Process -Name Explorer
         Write-Host “IE Enhanced Security has been enabled.” -ForegroundColor Green
     }
