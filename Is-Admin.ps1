439dec48cf58ddd46b126c9d36b2dc607179d690
Stefan Zawacki <s.zawacki@oraylis.de>
2016-05-24 07:59:05 +0200

    Update Is-Admin.ps1

--- Is-Admin.ps1
+++ Is-Admin.ps1
@@ -1,5 +1,24 @@
 Function Is-Admin
 {
-    # Returns true/false
+    <#
+
+        .SYNOPSIS
+
+        Check if current user is member of local administrators group.
+
+
+
+        .DESCRIPTION
+
+        This function checks if the user running the script is amember of the local administrators group.
+
+
+
+        .EXAMPLE 
+
+        Is-Admin
+
+    #>
+
     ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 }
