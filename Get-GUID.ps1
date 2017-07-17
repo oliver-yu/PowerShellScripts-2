057923b8d93874f398a6511572c067e8a9780d29
Stefan Zawacki <Stefan Zawacki>
2017-07-17 15:52:50 +0200

    Add Get-GUID.ps1

---
+++ Get-GUID.ps1
@@ -0,0 +1,50 @@
+function Get-Guid
+{
+    <#
+        .SYNOPSIS
+            Generates a new GUID.
+
+        .DESCRIPTION
+            Generates a new 32 Bit GUID.
+
+        .PARAMETER AddPraces
+            Adds braces surrounding the GUID.
+
+        .PARAMETER RemoveHyphen
+            Removes the hyphens.
+
+        .PARAMETER Uppercase
+            Returns only upper case letters.
+
+        .EXAMPLE 
+            Get-GUID
+            Get-GUID -AddBraces -Uppercase
+
+    #>
+
+    Param([switch]$AddBraces,
+    [switch]$RemoveHyphens,
+    [switch]$Uppercase
+    )
+
+    $guid = (New-Guid).Guid
+
+    if ($Uppercase)
+    {
+        $guid = $guid.ToUpper()
+    }
+
+    if ($AddBraces)
+    {
+        $guid = '{' + $guid + '}'
+    }
+
+    if ($RemoveHyphens)
+    {
+        $guid = $guid.Replace('-', '')
+    }
+
+    $guid
+}
+
+Get-Guid
