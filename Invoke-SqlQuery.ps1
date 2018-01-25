603cafdb42b89ad65de9a4b6090619f74b1c1a82
Stefan Zawacki <Stefan.Zawacki@outlook.com>
2018-01-25 10:59:21 +0100

    Update Invoke-SqlQuery.ps1
    Add fix for server disconnect issue.

--- Invoke-SqlQuery.ps1
+++ Invoke-SqlQuery.ps1
@@ -1,41 +1,29 @@
 Function Invoke-SqlQuery
 {
     <#
-
         .SYNOPSIS
 
         Execute sql query.
 
-
-
         .DESCRIPTION
 
         Short function to execute an sql query.
-        
-
 
         .PARAMETER SqlServerName 
 
         Name of the sql server to connect to.
 
-
-
         .PARAMETER QueryString 
 
         Sql query to be executed.
-
-
 
         .EXAMPLE 
 
         Invoke-SqlQuery -SqlServerName "SqlServer001" -QueryString "SELECT TOP 1000 * FROM [Db1].[dbo].[Table1]"
 
-
-
         .NOTES
 
         Links: https://msdn.microsoft.com/en-us/library/microsoft.sqlserver.management.common.serverconnection.aspx 
-
     #>
 
     [cmdletbinding()]
@@ -51,19 +39,21 @@ Function Invoke-SqlQuery
     # Reqiures Microsoft.SqlServer.ConnectionInfo.dll; See link above.
     [void][reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.ConnectionInfo")
     
-    $ServerConnection = New-Object Microsoft.SqlServer.Management.Common.ServerConnection($SqlServerName)
+    $serverConnection = New-Object Microsoft.SqlServer.Management.Common.ServerConnection($SqlServerName)
 
     try 
     {
-        $Reader = $ServerConnection.ExecuteReader($QueryString)
+        $reader = $ServerConnection.ExecuteReader($QueryString)
         Write-Host "Query '$QueryString' successfully executed." -ForegroundColor Cyan
-        return $Reader
+        $result = $reader | ForEach-Object {$a = @{}; for ($i = 0; $i -lt $Reader.FieldCount; $i++) { $a.Add($Reader.GetName($i), $_.GetValue($i))}; $a}
+	    $serverConnection.Disconnect()
+        return $result
     } 
     catch 
     {
         Write-Host $_.Exception.ToString() -ForegroundColor Red
-        $Reader.Close()
-        $Reader.Dispose()
+        $reader.Close()
+        $reader.Dispose()
         return $null
     }    
 }
